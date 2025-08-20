d8a7f34a58414fea0930a5a912f4c1a6f159fb07
Mathew Punnen Chandy <mathewpunnen8281@gmail.com>
2025-08-20 23:55:52 +0530

    feat: implement letter input handling and keyboard events in Wordle component

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -3,6 +3,7 @@ import { useState, useRef, useEffect } from "react";
 import "../styles/Wordle.scss";
 import Row from "./Row";
 import Keyboard from "./Keyboard";
+import { LETTERS, potentialWords } from "../data/LettersWords";
 
 const SOLUTION = "react";
 
@@ -16,13 +17,64 @@ const Wordle = () => {
     "     ",
   ]);
 
+  const [solutionFound, setSolutionFound] = useState(false);
+  const [activeLetterIndex, setActiveLetterIndex] = useState(0);
+  const [notification, setNotification] = useState("");
+  const [activeRowIndex, setActiveRowIndex] = useState(0);
+
   const wordleRef = useRef();
 
   useEffect(() => {
     wordleRef.current.focus();
   }, []);
 
-  const handleKeyDown = () => {};
+  const typeLetter = (letter) => {
+    if (activeLetterIndex < 5) {
+      setNotification("");
+
+      let newGuesses = [...guesses];
+      newGuesses[activeRowIndex] = replaceCharacter(
+        newGuesses[activeRowIndex],
+        activeLetterIndex,
+        letter
+      );
+
+      setGuesses(newGuesses);
+      setActiveLetterIndex((index) => index + 1);
+    }
+  };
+
+  const replaceCharacter = (string, index, replacement) => {
+    return (
+      string.slice(0, index) +
+      replacement +
+      string.slice(index + replacement.length)
+    );
+  };
+
+  const hitEnter = () => {
+    //TODO
+  };
+
+  const hitBackspace = () => {
+    //TODO
+  };
+
+  const handleKeyDown = (event) => {
+    if (solutionFound) return;
+    if (LETTERS.includes(event.key)) {
+      typeLetter(event.key);
+    }
+
+    if (event.key === "Enter") {
+      hitEnter();
+      return;
+    }
+
+    if (event.key === "Backspace") {
+      hitBackspace();
+    }
+  };
 
   return (
     <div
